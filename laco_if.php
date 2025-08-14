2b6be1754328b450b9b702401c92962571f3517e
Yago <pavanyago98@gmail.com>
2025-08-14 11:12:24 -0300

    adição do laço

---
+++ laco_if.php
@@ -0,0 +1,10 @@
+<?php
+    $resposta = "não";
+    $pergunta = "Sua mãe deixou você ir no Mcdonald?";
+
+    if ($resposta == "sim"){
+        echo "O rolê esta on";
+}else{ echo "Hoje não tera rolê";
+    }
+
+?>
