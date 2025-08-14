ff714be9d9df1950002ac5306503dffe8e28f611
Yago <pavanyago98@gmail.com>
2025-08-14 11:40:16 -0300

    um if eum else

--- laco_if.php
+++ laco_if.php
@@ -7,4 +7,12 @@
 }else{ echo "Hoje não tera rolê";
     }
 
+    echo "<br>";
+    $numero1 = 100;
+    $numero2 =50;
+    
+    if ($numero1 > $numero2){
+        echo "O $numero1 é maior que o $numero2 !";
+    }else{ echo "O $numero2 é maior que o $numero1 !";
+    }
 ?>
