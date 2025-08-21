64b95cb3dd890e5a0f60eba2cad84fe32a8c3f0e
Yago <pavanyago98@gmail.com>
2025-08-21 15:57:14 -0300

    final dia 21/06/2025

---
+++ while.php
@@ -0,0 +1,13 @@
+<h3> Este é um while loop </h3>
+<?php   
+    $contador = 1;
+    while($contador <= 5){
+        echo "Número: $contador <br>";
+        $contador++;
+    }
+    $contador = 1;
+    do {
+        echo "Número: $contador <br>";
+        $contador++;
+    } while($contador <= 5);
+?>çç
